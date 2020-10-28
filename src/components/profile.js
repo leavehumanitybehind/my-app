722ccc0bd7f3f07ed6e7c1782767463eaff7716a
ubitinonevami <bernastya@inbox.ru>
2020-10-28 12:40:17 +0300

    Layout

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Profile = () => {
     return <div className="content">
-        <img src='https://i.pinimg.com/564x/68/21/29/68212953ff626b6351e69d414075b6c2.jpg' />
         <ul>
              <li>Ava</li>
         <li>Post</li>
