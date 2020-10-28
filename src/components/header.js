a170c6225b39f2ea24848592c4855d371d434c7b
ubitinonevami <bernastya@inbox.ru>
2020-10-28 12:07:53 +0300

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+const Header = () => {
+return <header className="header">
+    <img src='https://i.pinimg.com/564x/f9/78/cf/f978cf7a1eca9823156fdfc3af8b9e99.jpg' width="70" height="70" alt='logo' />
+    </header>
+};
+
+export default Header;
---
+++ src/components/profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+        <img src='https://i.pinimg.com/564x/68/21/29/68212953ff626b6351e69d414075b6c2.jpg' />
+        <ul>
+             <li>Ava</li>
+        <li>Post</li>
+        <li>Comments</li>
+            </ul>
+       
+    </div>
+}
+
+export default Profile;
